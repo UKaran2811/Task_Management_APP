edc23fb9bd37ee4d272c3db28fc208908f7fa17e
UKaran2811 <144004339+UKaran2811@users.noreply.github.com>
2026-06-14 13:46:08 +0530

    created all task and one task, get and point

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.tasks.models import TaskModel
+from fastapi import HTTPException
 
 
 def create_task(body:TaskSchema, db:Session):
@@ -14,3 +15,14 @@ def create_task(body:TaskSchema, db:Session):
     db.refresh(new_task)
 
     return {"status":"Task Created Successfully!", "data":new_task}
+
+
+def get_tasks(db:Session):
+    tasks = db.query(TaskModel).all()
+    return {"status":"All Tasks","data":tasks}
+
+def get_one_task(task_id:int, db:Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        raise HTTPException(404, detail="Task id is incorrect!")
+    return{"status":"Task Fetched Successfully", "data": one_task}
