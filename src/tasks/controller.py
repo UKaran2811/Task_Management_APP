fe87bf687cddb26f5d51f187d4bc51217fa063a2
UKaran2811 <144004339+UKaran2811@users.noreply.github.com>
2026-06-14 13:22:57 +0530

    Add get_tasks endpoint to retrieve all tasks

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -14,3 +14,8 @@ def create_task(body:TaskSchema, db:Session):
     db.refresh(new_task)
 
     return {"status":"Task Created Successfully!", "data":new_task}
+
+
+def get_tasks(db:Session):
+    tasks = db.query(TaskModel).all()
+    return {"status":"All Tasks","data":tasks}
